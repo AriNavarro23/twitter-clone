868e055da80f1a1028f0ed7d75bc65ef2ae4b98a
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-08-25 12:13:53 -0300

    commit

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { SearchIcon } from '@heroicons/react/outline';
+import React from 'react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 function Widgets() {
     return (
-        <div>Widgets</div>
+        <div className='mt-2 px-2'>
+            { /* serach */}
+            <div className='flex items-center space-x-2 bg-gray-100
+            p-3 mt-2 rounded-full'>
+                <SearchIcon className='h-5 w-5 text-gray-400' />
+                <input
+                    type='text'
+                    placeholder='Search Twitter'
+                    className='flex-1 bg-transparent outline-none'
+                />
+            </div>
+            <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="naar059123"
+                    options={{ height: 1000 }}
+                />
+        </div>
     )
 }
 
